45ad70aa46847ed70c5893e643c1130e91117b1b
YeoJiSu <duwltn1301@naver.com>
2021-10-11 14:15:36 +0900

    Store & Return various types of Object

---
+++ 6-1.Generic/practice_6-3/DataRepository.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRepository {
+    private List<Object> data = new ArrayList<>();
+    private static DataRepository instance = new DataRepository();
+    public static DataRepository getInstance() {
+        return instance;
+    }
+    public void add(Object data) {
+        this.data.add(data);
+    }
+    public <T> List<T> getData(Class<T> templateClass) {
+        List<T> newData = new ArrayList<>();
+        for (Object o :data){
+            if ( templateClass.isInstance(o) )
+                newData.add((T) o);
+        }
+        return newData;
+    }
+
+}
