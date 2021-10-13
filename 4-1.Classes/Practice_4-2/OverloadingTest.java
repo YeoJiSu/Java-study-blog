cc218e8c4fb1d9c5050325eb5099594b724b3a03
YeoJiSu <duwltn1301@naver.com>
2021-10-13 22:33:00 +0900

    Feat: Input Points & Output :white_medium_square: & moved :white_medium_square:

---
+++ 4-1.Classes/Practice_4-2/OverloadingTest.java
@@ -0,0 +1,16 @@
+public class OverloadingTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Rectangle rectangle1 = new Rectangle(new Point(sc.nextInt(), sc.nextInt()),
+                new Point(sc.nextInt(), sc.nextInt()));
+        Rectangle rectangle2 = new Rectangle(new Point(sc.nextInt(), sc.nextInt()),
+                new Point(sc.nextInt(), sc.nextInt()));
+
+        rectangle1.moveBy(sc.nextInt(), sc.nextInt());
+        rectangle2.moveBy(sc.nextInt());
+        sc.close();
+
+        System.out.println(rectangle1);
+        System.out.println(rectangle2);
+    }
+}
