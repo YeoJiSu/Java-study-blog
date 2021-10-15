40518fb6baf9cbfbfd1c2a5fe4475f4a43531777
YeoJiSu <duwltn1301@naver.com>
2021-10-16 00:00:47 +0900

    Inherit Player Class

---
+++ 6-1.Generic/practice_6-5/MidFielder.java
@@ -0,0 +1,13 @@
+public class MidFielder extends Player{
+
+
+
+    @Override
+    protected int getSpeed() {
+        return super.speed;
+    }
+    @Override
+    public String toString(){
+        return super.toString();
+    }
+}
